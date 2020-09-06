50697544af8bd1e90658ac001b6db12baf427bbe
AmenaAkter772 <amenavrt@gmail.com>
2020-09-06 22:55:01 +0600

    added everything

---
+++ src/Components/FakeData/FakeData.js
@@ -0,0 +1,8 @@
+const array = [
+    {image: "https://i.pinimg.com/originals/73/37/c0/7337c03b7b17e1c5d38ecfa1a35a055a.jpg",id:1},
+    {image: "https://images.indianexpress.com/2020/01/mumbai-girl12.jpg",id:2},
+    {image: "https://images.indianexpress.com/2020/01/mumbai-girl12.jpg",id:3},
+    
+]
+
+export default array;
